33a42de59dee350a71eec9e73092ae1f0469af60
bhaskar sengupta <dpaddr01-github@yahoo.com.au>
2022-11-12 11:26:12 -0800

    adding Pair

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bh45k4r/golang-dss
+
+go 1.18
---
+++ pair/pair.go
@@ -0,0 +1,19 @@
+package pair
+
+
+type Pair[K, V any] struct {
+  key K
+  value V
+}
+
+func New[K, V any](key K, value V) Pair[K, V] {
+  return Pair[K, V]{key, value}
+}
+
+func (p Pair[K, V]) Key() K {
+  return p.key
+}
+
+func (p Pair[K, V]) Value() V {
+  return p.value
+}
